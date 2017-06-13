a6fc72ae7bd2f48616160f1e4c640f4e6a339373
Janek Brandt and Mark Fisher <mark@>
2017-06-13 15:29:32 -0700

    add camera functionality and fix index swapping method

--- app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListFragment.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -93,6 +94,7 @@ public class SavedRestaurantListFragment extends Fragment implements OnStartDrag
     //method is now public
     public void onDestroy() {
         super.onDestroy();
+        Log.d("test", "cleanup");
         mFirebaseAdapter.cleanup();
     }
 }
